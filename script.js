5b9f768a7e935fe42f7e3eb975298256367b1c18
David R Tarvin Jr <david.tarvin@gmail.com>
2018-11-15 21:52:49 -0600

    Copy js from codeine

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function toggle_visibility(cl) {
-    console.log("========>cl is ", cl);
-    var elements = document.getElementsByClassName("showInfo");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.display = "none";
-    }
-    document.getElementById(cl).style.display = "block";
-} ;
+function toggle_visibility(cl){
+  console.log("========>cl is ", cl);
+  var elements=document.getElementsByClassName("showInfo");
+  for (i=0; i<elements.length; i++) {
+    elements[i].style.display="none";
+  }
+  document.getElementById(cl).style.display="block";
+};
 
-$("nav").find("a").click(function (e) {
+$("nav").find("a").click(function(e) {
     e.preventDefault();
     var section = $(this).attr("href");
     $("html, body").animate({
